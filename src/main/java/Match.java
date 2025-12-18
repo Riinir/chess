910b00937d511784f703127acaaf280724ad6cfb
ryannguyen <ryanmhnguyen@gmail.com>
2025-12-17 19:21:21 -0700

    added empty player and match classes

---
+++ src/main/java/Match.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Match {
+    private Player player1;
+    private Player player2;
+    ArrayList<Piece> blackPieces = new ArrayList<>();
+    ArrayList<Piece> whitePieces = new ArrayList<>();
+}
